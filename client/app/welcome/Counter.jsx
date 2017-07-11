2e8b85b64a49bb5a0e99fe59aeaf62c3bb3f25c6
Miguel Savignano <migue.masx@gmail.com>
2017-07-11 02:12:20 +0200

    use generator function to setup the initial state for mobx store

--- client/app/welcome/Counter.jsx
+++ client/app/welcome/Counter.jsx
@@ -3,37 +3,41 @@ import ReactOnRails from 'react-on-rails';
 import {observable} from 'mobx';
 import {observer} from 'mobx-react'
 
-const appState = observable({
-  count: 0
-})
+const App = (railsProps, railsContext) => {
+  const appState = observable({
+    count: railsProps.initialCount
+  })
 
-appState.increment = function () {
-  this.count++;
-}
-
-appState.decrement = function () {
-  this.count--;
-}
-
-@observer class Counter extends React.Component{
-  render() {
-    return(
-      <div>
-        Counter: {appState.count} <br/>
-        <button onClick={this.handleInc}> + </button>
-        <button onClick={this.handleDec}> - </button>
-      </div>
-    );
+  appState.increment = function () {
+    this.count++;
   }
 
-  handleInc = () => {
-    appState.count++;
+  appState.decrement = function () {
+    this.count--;
   }
 
-  handleDec =() => {
-    appState.count--;
+  @observer class Counter extends React.Component{
+    render() {
+      return(
+        <div>
+          Counter: {appState.count} <br/>
+          <button onClick={this.handleInc}> + </button>
+          <button onClick={this.handleDec}> - </button>
+        </div>
+      );
+    }
+
+    handleInc = () => {
+      appState.count++;
+    }
+
+    handleDec =() => {
+      appState.count--;
+    }
   }
+  // The generator function 'App' need to return a react element
+  return <Counter />
 }
 
 // This is how react_on_rails can see the Counter in the browser.
-ReactOnRails.register({ Counter });
+ReactOnRails.register({ Counter: App });
